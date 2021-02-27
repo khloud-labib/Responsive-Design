38511ab89458491e2e4fd4db0800ea18d298e519
khloud labib <78904252+khloud-labib@users.noreply.github.com>
2021-02-27 22:58:00 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+	$(function (){
+		'use script';
+		$('.info-list li').click(function (){
+         $(this).addClass('selected').siblings('li').removeClass('selected');
+			$('.info-content div').hide();
+			$('.' + $(this).data('class')).fadeIn();
+		});
+	});
